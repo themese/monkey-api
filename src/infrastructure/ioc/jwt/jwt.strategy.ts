484c8e524b079dfac8727912fa014e8126ea792c
TheMese <jousema.fernandez@gmail.com>
2022-08-18 21:47:02 +0200

    Fixed jwt issuer

--- src/infrastructure/ioc/jwt/jwt.strategy.ts
+++ src/infrastructure/ioc/jwt/jwt.strategy.ts
@@ -14,12 +14,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `${process.env.AUTH0_DOMAIN}.well-known/jwks.json`,
+        jwksUri: `${process.env.AUTH0_ISSUER_URL}/.well-known/jwks.json`,
       }),
 
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       audience: process.env.AUTH0_AUDIENCE,
-      issuer: `${process.env.AUTH0_DOMAIN}`,
+      issuer: `${process.env.AUTH0_ISSUER_URL}`,
       algorithms: ['RS256'],
     });
   }
